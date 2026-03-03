d3139281b0e9679e9f165a4192aba53c8ca76a38
simonbf15 <barralesimon@gmail.com>
2026-03-03 20:12:37 -0300

    agrego python basico

---
+++ semana1/hola_mundo.py
@@ -0,0 +1,8 @@
+#este es un comentario
+
+print('hola mundo :)')
+
+#tipos de datos
+print(type('esto es texto'))
+print(type(3))
+print(type(4.0))
---
+++ semana1/variables.py
@@ -0,0 +1,20 @@
+#variables
+#int
+num0 = 4
+
+#float
+num1 = 6.3
+
+#suma
+resul = num0 + num1
+
+#mostrar resultado
+print (resul)
+
+#string
+mi_texto = "hola el resultado es: "
+#mostrar resultado + texto
+print (mi_texto , resul)
+
+mi_numero = 15
+print ("mi num favorito es: ", mi_numero)
